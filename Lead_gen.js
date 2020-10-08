352dfbe982f141a2e504f3effd7d396fd104c22c
Alexandre Bellaich <65114774+Scrpzr@users.noreply.github.com>
2020-10-08 19:30:42 +0200

    Update and rename script.js to Lead_gen.js

--- Lead_gen.js
+++ Lead_gen.js
@@ -13,13 +13,13 @@ document.addEventListener("DOMContentLoaded", event => {
         const Var1 = $('[name=name of parameter 1]').val()
         const Var2 = $('[name=name of parameter 2]').val()
         const Var3 = $('[name=name of parameter 3]').val()
-				//add as much var as you need
+	//add as much var as you need
         const VarX = $('[name=name of parameter X]').val()
 
-        // calculer le resultat en utilisant les const crées précédement
+        // Make your calcul with the const you create
         const Result = Var1 + Var2 * Var3 / VarX
 
-        // affiche le resultat dans le champs Valeur
+        // Display the result in your #result field
         const resultField = $('#result')
         resultField.text(`${Result}`)
         resultField.val(`${Result}`)
