b56c5792b55cb20d27373c536f251fe43192071e
Alexandre Bellaich <65114774+Scrpzr@users.noreply.github.com>
2020-10-08 19:37:46 +0200

    Update live-calculator.js

--- live-calculator.js
+++ live-calculator.js
@@ -1,1 +1,33 @@
+<script>
+
+document.addEventListener("DOMContentLoaded", event => {
+    //prevent form to submit
+    $('#form ID').submit(function (event) {
+    		  event.preventDefault()
+    		  return false })
+    //update results when changing a parameter
+    $('[name=name of parameter 1]').change(updateResult)
+    $('[name=name of parameter 2]').change(updateResult)
+    $('[name=name of parameter 3]').change(updateResult)
+		//add as much parameters as you need
+    $('[name=name of parameter X]').change(updateResult)
+
+    function updateResult() {
+        //Get values by names et create var
+        const Var1 = $('[name=name of parameter 1]').val()
+        const Var2 = $('[name=name of parameter 2]').val()
+        const Var3 = $('[name=name of parameter 3]').val()
+	//add as much var as you need
+        const VarX = $('[name=name of parameter X]').val()
+
+        // Make your calcul with the const you create
+        const Result = Var1 + Var2 * Var3 / VarX
+
+        // Display the result in your #result field
+        const resultField = $('#result')
+        resultField.text(`${Result}`)
+	    
+    }
+ })
+</script>
 
